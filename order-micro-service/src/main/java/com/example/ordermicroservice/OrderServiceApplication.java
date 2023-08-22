d764f885570a43c844aad9390cc025f800a2847a
Ajay123-web <ajaykumargupta35304@gmail.com>
2023-08-22 19:50:08 +0530

    Refactor

--- order-micro-service/src/main/java/com/example/ordermicroservice/OrderServiceApplication.java
+++ order-micro-service/src/main/java/com/example/ordermicroservice/OrderServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.ordermicroservice.demo;
+package com.example.ordermicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- order-micro-service/src/main/java/com/example/ordermicroservice/demo/repositry/OrderRepositry.java
+++
@@ -1,8 +0,0 @@
-package com.example.ordermicroservice.demo.repositry;
-
-import com.example.ordermicroservice.demo.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepositry extends JpaRepository<Order, Long> {
-
-}
